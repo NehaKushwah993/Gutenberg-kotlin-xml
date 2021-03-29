16eb3d325816e8dec82f694697865b7dc8df747c
Neha <62131967+NehaVerma993@users.noreply.github.com>
2021-03-30 00:32:11 +0530

    Opened browser on click on book

--- app/src/main/java/com/nehak/gutenberg_task/ui/adapters/BooksAdapter.kt
+++ app/src/main/java/com/nehak/gutenberg_task/ui/adapters/BooksAdapter.kt
@@ -1,12 +1,17 @@
 package com.nehak.gutenberg_task.ui.adapters
 
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.nehak.gutenberg_task.BuildConfig
 import com.nehak.gutenberg_task.databinding.ListItemBookBinding
 import com.nehak.gutenberg_task.models.Book
+
 
 /**
  * Created by Neha Kushwah on 3/29/21.
@@ -20,8 +25,22 @@ class BooksAdapter(private val context: Context, private val list: ArrayList<Boo
         private val listItemBookBinding: ListItemBookBinding
     ) : RecyclerView.ViewHolder(listItemBookBinding.root) {
         fun bind(book: Book) {
+            itemView.setOnClickListener {
+                openWebBrowserApp(context, book)
+            }
             listItemBookBinding.book = book
             listItemBookBinding.executePendingBindings()
+        }
+
+        private fun openWebBrowserApp(context: Context, book: Book) {
+            val urlString = BuildConfig.BOOK_WEB_URL + book.id
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            try {
+                context.startActivity(intent)
+            } catch (ex: ActivityNotFoundException) {
+                ex.printStackTrace()
+            }
         }
     }
 
